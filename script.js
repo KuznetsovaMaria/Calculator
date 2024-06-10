6a3d9790a861aa733f705817bbc01ed1d98b8e3b
KuznetsovaMaria <113997426+KuznetsovaMaria@users.noreply.github.com>
2024-06-10 11:15:40 +0500

    feat: add vars for operands and operator

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+let operand1, operator, operand2;
+
 function add(addend1, addend2) {
+    operand1 = addend1, operator = '+', operand2 = addend2;
     return addend1 + addend2;
 }
 
 function subtract(minuend, subtrahend) {
+    operand1 = minuend, operator = '-', operand2 = subtrahend;
     return minuend - subtrahend;
 }
 
 function multiply(multiplicand, multiplier) {
+    operand1 = multiplicand, operator = '*', operand2 = multiplier;
     return multiplicand * multiplier;
 }
 
 function divide(dividend, divisor) {
+    operand1 = dividend, operator = '/', operand2 = divisor;
     if (divisor === 0) {
         return 'nope'
     }
