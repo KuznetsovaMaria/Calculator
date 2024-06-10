e17f899a4953b1b45f6602e2d77b3afba0167093
KuznetsovaMaria <113997426+KuznetsovaMaria@users.noreply.github.com>
2024-06-10 11:05:36 +0500

    feat: add funcs for 4 basic math operators

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(addend1, addend2) {
+    return addend1 + addend2;
+}
+
+function subtract(minuend, subtrahend) {
+    return minuend - subtrahend;
+}
+
+function multiply(multiplicand, multiplier) {
+    return multiplicand * multiplier;
+}
+
+function divide(dividend, divisor) {
+    if (divisor === 0) {
+        return 'nope'
+    }
+    return Math.round(dividend / divisor);
+}
