55b34bc15093ed0a474f7e1a0f86b99273899337
KuznetsovaMaria <113997426+KuznetsovaMaria@users.noreply.github.com>
2024-06-10 11:30:49 +0500

    feat: add func for all operators

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let operand1, operator, operand2;
 
+function operate(operand1, operator, operand2) {
+    switch(operator) {
+        case '+' :
+            return add(operand1, operand2);
+        case '-' :
+            return subtract(operand1, operand2);
+        case '*' :
+            return multiply(operand1, operand2);
+        case '/' :
+            return divide(operand1, operand2);
+    }
+}
+
 function add(addend1, addend2) {
     operand1 = addend1, operator = '+', operand2 = addend2;
     return addend1 + addend2;
