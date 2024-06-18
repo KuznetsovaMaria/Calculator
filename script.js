c322c0db9fef6198bc9d7e71051b0dee44a4b153
KuznetsovaMaria <113997426+KuznetsovaMaria@users.noreply.github.com>
2024-06-18 20:48:11 +0500

    feat: connect calc interface with full functionality

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
-let operand1, operator, operand2;
+const display = document.querySelector('.display');
+const btnClear = document.querySelector('#clearAllBtn');
+const calcBtns = document.querySelector('.calculator');
 
-function operate(operand1, operator, operand2) {
+let displayedStr;
+let inputValues;
+
+calcBtns.addEventListener('click', (e) => {
+    if (e.target.textContent.match(/[0-9]/)) {
+        display.textContent += e.target.textContent;
+    } else if (e.target.textContent.match(/[+-/*]/)) {
+        isSecondOperation(display.textContent);
+        display.textContent += e.target.textContent;
+    } else if (e.target.textContent.match(/C/)) {
+        display.textContent = '';
+        inputValues = [];   
+    } else if (e.target.textContent.match(/=/)) {
+        displayedStr = display.textContent;
+        display.textContent += e.target.textContent;
+        convertToOperation(displayedStr);
+    }
+})
+
+function isSecondOperation(str) {
+    if (str.match(/[^0-9]/)) {
+        displayedStr = str;
+        convertToOperation(displayedStr);
+    }
+}
+
+function convertToOperation(str) {
+    if (str.includes('+')) {
+        inputValues = displayedStr
+            .split('+')
+            .map((char) => Number(char));
+        inputValues.push('+');
+    } else if (str.includes('*')) {
+        inputValues = displayedStr
+            .split('*')
+            .map((char) => Number(char));
+        inputValues.push('*');
+    } else if (str.includes('/')) {
+        inputValues = displayedStr
+            .split('/')
+            .map((char) => Number(char));
+        inputValues.push('/');
+    } else if (str.includes('-')) {
+        inputValues = displayedStr
+            .split('-')
+            .map((char) => Number(char));
+        inputValues.push('-');
+    }
+    let answer = operate(...inputValues);
+    display.textContent = answer;
+}
+
+function operate(operand1, operand2, operator) {
     switch(operator) {
         case '+' :
             return add(operand1, operand2);
@@ -14,22 +68,18 @@ function operate(operand1, operator, operand2) {
 }
 
 function add(addend1, addend2) {
-    operand1 = addend1, operator = '+', operand2 = addend2;
     return addend1 + addend2;
 }
 
 function subtract(minuend, subtrahend) {
-    operand1 = minuend, operator = '-', operand2 = subtrahend;
-    return minuend - subtrahend;
+    return (minuend - subtrahend);
 }
 
 function multiply(multiplicand, multiplier) {
-    operand1 = multiplicand, operator = '*', operand2 = multiplier;
     return multiplicand * multiplier;
 }
 
 function divide(dividend, divisor) {
-    operand1 = dividend, operator = '/', operand2 = divisor;
     if (divisor === 0) {
         return 'nope'
     }
